814fe7a618dfc29ebce75f9e84c254cf8844137b
unknown <lwrow@rambler.ru>
2025-03-04 19:34:45 +0300

    created and linked Timer bean

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -11,11 +11,19 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
+    @Qualifier("timer")
+    private Timer timer = new Timer();
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
